7045617c0aa75652cbe61807b984dc2937d100b4
Paulo <paulorfsantos17@gmail.com>
2023-10-31 16:55:15 -0300

    feat: add banner discount to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-.01.png"
+        alt="até 55% de desconto este mês"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      ></Image>
+    </div>
+  );
+}
