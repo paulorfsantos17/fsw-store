0bbc642540057148b681f9ef70a25678d7ae38d1
Paulo <paulorfsantos17@gmail.com>
2023-11-02 12:12:07 -0300

    fix: unsure user goes to category page on categoryItem Click

--- src/app/(home)/components/categoryItem.tsx
+++ src/app/(home)/components/categoryItem.tsx
@@ -2,6 +2,7 @@
 import { Badge } from "@/components/ui/badge";
 import { CATEGORY_ICONS } from "@/constants/category-icons";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -9,13 +10,15 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <Badge
-      variant="outline"
-      className="flex justify-center gap-2 rounded-lg py-3"
-    >
-      {CATEGORY_ICONS[category.slug as keyof typeof CATEGORY_ICONS]}
-      <span className="text-bold text-xs">{category.name}</span>
-    </Badge>
+    <Link href={`/category/${category.slug}`}>
+      <Badge
+        variant="outline"
+        className="flex justify-center gap-2 rounded-lg py-3"
+      >
+        {CATEGORY_ICONS[category.slug as keyof typeof CATEGORY_ICONS]}
+        <span className="text-bold text-xs">{category.name}</span>
+      </Badge>
+    </Link>
   );
 };
 
