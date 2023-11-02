f7d9b83e04fa660eee9975cc4161150e47a5fe3f
Paulo <paulorfsantos17@gmail.com>
2023-11-02 10:11:51 -0300

    chore: add url domains image keyboards

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {  images: {
-  domains: [
-    "fsw-store.s3.sa-east-1.amazonaws.com",
-  ],
-},}
+const nextConfig = {
+  images: {
+    domains: [
+      "fsw-store.s3.sa-east-1.amazonaws.com",
+      "uploaddeimagens.com.br"
+    ],
+  },
+}
 
 module.exports = nextConfig
