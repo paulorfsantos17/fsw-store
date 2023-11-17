5bce63b4fb369aec073074001544095dae3a5adc
Paulo <paulorfsantos17@gmail.com>
2023-11-16 21:17:41 -0300

    feat: ensure orderStatus show correct

--- src/app/orders/components/orderItem.tsx
+++ src/app/orders/components/orderItem.tsx
@@ -8,6 +8,15 @@ import { Card } from "@/components/ui/card";
 import { Prisma } from "@prisma/client";
 import { format } from "date-fns";
 import OrderProductItem from "./orderProductsItem";
+import { Separator } from "@/components/ui/separator";
+import OrderResultsTitle from "./orderResultsTitle";
+import { useMemo } from "react";
+import {
+  computeSubtotalProductsToCart,
+  computeTotalProductsToCart,
+  computeTotalProductsToOrderProducts,
+} from "@/helpers/product";
+import { getOrderStatus } from "../helpers/status";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
@@ -22,13 +31,25 @@ interface OrderItemProps {
 }
 
 const OrderItem = ({ order }: OrderItemProps) => {
+  const subTotal = useMemo(() => {
+    return computeSubtotalProductsToCart(order.orderProducts);
+  }, [order.orderProducts]);
+
+  const total: number = useMemo(() => {
+    return computeTotalProductsToOrderProducts(order.orderProducts);
+  }, [order.orderProducts]);
+
+  const totalDiscount = subTotal - total;
   return (
     <Card className="px-5">
       <Accordion type="single" className="w-full" collapsible>
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              Pedido com {order.orderProducts.length} produto(s)
+              <p>Pedido com {order.orderProducts.length} produto(s)</p>
+              <span className="text-sm opacity-60">
+                Feito em {format(order.createAt, "d/MM/y 'às'HH:mm")}
+              </span>
             </div>
           </AccordionTrigger>
           <AccordionContent>
@@ -36,7 +57,10 @@ const OrderItem = ({ order }: OrderItemProps) => {
               <div className="flex items-center justify-between">
                 <div className="font-bold">
                   <p className="uppercase">Status</p>
-                  <p className="text-[#8162FF]"> {order.status}</p>
+                  <p className="text-[#8162FF]">
+                    {" "}
+                    {getOrderStatus(order.status)}
+                  </p>
                 </div>
                 <div>
                   <p className="font-bold uppercase">Data</p>
@@ -55,6 +79,22 @@ const OrderItem = ({ order }: OrderItemProps) => {
                   key={orderProduct.id}
                 />
               ))}
+
+              <div className="flex flex-col gap-1">
+                <Separator />
+                <OrderResultsTitle
+                  title="Subtotal"
+                  value={`R$ ${subTotal.toFixed(2)}`}
+                />
+                <Separator />
+                <OrderResultsTitle title="Entrega" value="GRÁTIS" />
+                <Separator />
+                <OrderResultsTitle
+                  title="Total"
+                  value={`R$ ${total.toFixed(2)}`}
+                  bold={true}
+                />
+              </div>
             </div>
           </AccordionContent>
         </AccordionItem>
